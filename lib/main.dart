9aca96de9ec6a3027c2b53f10b282143a9dfde12
jonathanleivag <email@jonathanleivag.cl>
2021-12-19 16:56:42 -0300

    inicialización de ios

--- lib/main.dart
+++ lib/main.dart
@@ -43,33 +43,3 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark());
   }
 }
-
-class Splash extends StatelessWidget {
-  const Splash({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    bool lightMode =
-        MediaQuery.of(context).platformBrightness == Brightness.light;
-    return Scaffold(
-      backgroundColor:
-          lightMode ? const Color(0xffe1f5fe) : const Color(0xff042a49),
-      body: Center(
-          child: lightMode
-              ? Image.asset('assets/loading.gif')
-              : Image.asset('assets/loading.gif')),
-    );
-  }
-}
-
-class Init {
-  Init._();
-  static final instance = Init._();
-
-  Future initialize() async {
-    // This is where you can initialize the resources needed by your app while
-    // the splash screen is displayed.  Remove the following example because
-    // delaying the user experience is a bad design practice!
-    await Future.delayed(const Duration(seconds: 3));
-  }
-}
